de8a1cf688414361098eb5fc61e78dfacb371a56
annelaka <ubuntu@ip-172-31-65-156.ec2.internal>
2022-01-05 08:27:35 +0000

    [WIP] Working version with dataset versioning and data master.

--- readers/ende_ds.py
+++ readers/ende_ds.py
@@ -1,25 +1,32 @@
-from .utils.text_clean import filter_long_short, fix_bad_token, filter_bad_tag
+from ...utils.text_clean import filter_long_short, fix_bad_token, filter_bad_tag
 import datasets
+from pathlib import Path
 
 class EnDe(object):
-    def __init__( self, version_str, source_dir, cache_dir=None ):
+    def __init__( self, version_str, data_dir, write_mode=False ):
         
         self.version_str = version_str
-        self.source_dir = source_dir
-        self.cache_dir = cache_dir
-        self.download_cmd = 'mtdata signature used to download and organize data.'
+        self.data_dir = data_dir
+        self.write_mode = write_mode
+        self.download_cmd = ''
 
-        if version_str == 'BASIC_CLEANING':
-            self.dataset = self.get_revision_basic_cleaning ( source_dir, cache_dir )
-        else:
-            raise ValueError( f'Version string {version_str} not found.' )
+        self.Version2Flow_Mapping = {
+            'BASIC_CLEANING' : self.writerevision_basic_cleaning
+        }
 
-    def get_revision_basic_cleaning( self, source_dir, cache_dir ):
+        self.target_dir = Path(self.data_dir, version_str)
+        if not self.write_mode :
+            if not self.target_dir.exists() :
+                raise ValueError(f'Path {self.target_dir} does not exist for reading.')
+            self.dataset = datasets.load_from_disk( self.target_dir )
+        else : # writing data to disk
+            
+            flow_executor = self.Version2Flow_Mapping[version_str]
+            self.dataset =  flow_executor( self.data_dir, self.target_dir )
+
+    def writerevision_basic_cleaning( self, source_dir, target_dir ):
         # read dataset
-        hf_dataset = datasets.load_dataset(
-            path=self.source_dir, 
-            cache_dir=self.cache_dir,
-            )
+        hf_dataset = datasets.load_dataset( path=source_dir )
 
         # rename columns and concatenate
         hf_dataset['en'] = hf_dataset['en'].rename_column('text', 'en')
@@ -37,5 +44,6 @@ class EnDe(object):
             .filter ( lambda s: filter_long_short(s['en_clean'].split(' '), 1, 25) ) \
             .filter ( lambda s: filter_long_short(s['de_clean'].split(' '), 1, 25) )
 
+        hf_dataset.save_to_disk( target_dir )
         return hf_dataset
 
